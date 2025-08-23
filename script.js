63b7dcbb6186a20cd3669b496c436fd4ec7431da
Guilherme <guilhermedevfull@gmail.com>
2025-08-23 19:23:38 -0300

    added the function to choose the human player in the game

--- script.js
+++ script.js
@@ -8,4 +8,18 @@ function computerChoice() {
 let escolha = computerChoice();
 console.log('Choice of computer: ' + escolha);
 
+function getHumanChoice(){
+   
+    let choiceHuman = prompt("choose one of the options: rock, paper or scissors");
+    return choiceHuman;
+}
 
+let myChoice = getHumanChoice();
+console.log('You chose: ' + myChoice);
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice , computerChoice){
+    if (humanChoice === )
+}
